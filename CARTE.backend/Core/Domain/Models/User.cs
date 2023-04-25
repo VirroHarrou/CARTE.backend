eebb2384b5ac2af77bb5906ea8f4880c88a1914c
VirroHarrou <81008781+VirroHarrou@users.noreply.github.com>
2023-04-25 15:12:16 +0300

    changed connection string

--- CARTE.backend/Core/Domain/Models/User.cs
+++ CARTE.backend/Core/Domain/Models/User.cs
@@ -7,7 +7,6 @@
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string? ImageUrl { get; set; }
         public ICollection<BusinessCard> Cards { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
